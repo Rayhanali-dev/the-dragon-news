a078acb91a80657beabb8a12bdb707fe978aeb32
Rayhanali-dev <rayhanalidev@gmail.com>
2023-05-01 01:32:22 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 const catagories = require('./data/categories.json')
+const news = require('./data/newsdata.json')
 
 app.use(cors())
 
@@ -12,6 +13,27 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(catagories)
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n => n._id == id)
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id == 0) {
+        res.send(news);
+    } else {
+        const catagoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(catagoryNews)
+    }
 })
 
 app.listen(port, () => {
